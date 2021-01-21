3c21e9c89ea125b1d8ac73efded5a9fe76871875
Win Zaw Oo <iwinzawcool@gmail.com>
2021-01-21 15:35:31 +0630

    remove some code

--- scripts.py
+++ scripts.py
@@ -1,23 +1,5 @@
-import math
-import os
-import sys
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = f"Hello, {who_to_greet}"
-    return greeting
-
-
-print(greet("World"))
-print(greet("Corey"))
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
-
-
-# name = input('Your name?')
-# print('Hello,', name)
+print(r.ok)
